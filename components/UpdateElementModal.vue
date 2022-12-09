efcbb8a40a9421c1d05b95f9f838384a47f4a4cb
jonjav1020 <tech7@sumomedia.co>
2022-12-09 12:21:43 +0900

    Fix Edit Element Image part

--- components/UpdateElementModal.vue
+++ components/UpdateElementModal.vue
@@ -35,9 +35,88 @@
                 <label class="my-8 block">
                     <!-- todo: insert croppa here -->
                     <VImg
-                        :src="thumbnail"
+                      v-if="showInitialImage"
+                      :src="`${$config.baseURL}/storage/${thumbnail}`"
                     ></VImg>
-                </label>
+                    <br />
+                    <div class="mx-auto my-9 flex items-center justify-center">
+                        <Croppa
+                            v-model="elementCroppa"
+                            :auto-sizing="true"
+                            placeholder="Attach image"
+                            :quality="2"
+                            accept=".png, .webp, .jpeg, .jpg"
+                            :image-border-radius="60"
+                            :zoom-speed="4"
+                            :placeholder-font-size="18"
+                            initial-size="contain"
+                            show-loading="true"
+                            @file-type-mismatch="handleFileTypeMismatch"
+                            @new-image-drawn="handleNewImage"
+                            @image-remove="handleRemoveImage"
+                        >
+                        </Croppa>
+                    </div>
+                    <section class="flex flex-col">
+                        <div class="flex items-center justify-center">
+                            <VBtn small @click="rotateAnti">
+                                <i class="ri-anticlockwise-line"></i>
+                            </VBtn>
+                            <VBtn small @click="rotate">
+                                <i class="ri-clockwise-line"></i>
+                            </VBtn>
+                            <VBtn small @click="flipx">
+                                <i class="ri-arrow-left-right-line"></i>
+                            </VBtn>
+                            <VBtn small @click="flipy">
+                                <i class="ri-arrow-up-down-line"></i>
+                            </VBtn>
+                        </div>
+                        <div class="flex items-center justify-center">
+                            <VBtn small @click="zoomIn">
+                                <i class="ri-zoom-in-line"></i>
+                            </VBtn>
+                            <VBtn small @click="zoomOut">
+                                <i class="ri-zoom-out-line"></i>
+                            </VBtn>
+                        </div>
+                        <VBtn
+                          v-if="showGenerateBtn"
+                          dark
+                          large
+                          class="my-4 bg-gradient-to-r
+                          from-brand-black to-brand-black"
+                          @click="generateElementImage"
+                        >
+                          GENERATE
+                        </VBtn>
+                    </section>
+                  </label>
+                  <br />
+                  <div class="grid grid-cols-1 gap-3">
+                    <div class="relative col-span-1 flex justify-center">
+                      <VImg
+                        v-if="isImagePreview"
+                        :src="thumbnail"
+                      ></VImg>
+                      <button
+                        v-if="isImagePreview"
+                        type="button"
+                        class="
+                          absolute
+                          left-0 my-2
+                          h-6
+                          w-6 text-brand-lgrey
+                          shadow-sm
+                          hover:bg-brand-black
+                          hover:text-white
+                        "
+                        @click="removeImage()"
+                      >
+                        <div class="ri-close-fill ri-lg"></div>
+                      </button>
+                    </div>
+                  </div>
             </template>
             <template v-if="thumbnailType === 'color'">
                 <div
@@ -119,8 +198,12 @@ export default {
       elementName: '',
       thumbnailType: '',
       thumbnail: '',
+      initialThumbnail: '',
       imgValue: '',
       elementCroppa: {},
+      showGenerateBtn: false,
+      isImagePreview: false,
+      showInitialImage: true,
       items: [
         'Select one',
         'image',
@@ -145,6 +228,7 @@ export default {
   methods: {
     closeDialog() {
       this.$emit('close')
+      this.$emit('retrieve')
     },
     retrieveElement(elid) {
       this.$axios
@@ -168,16 +252,80 @@ export default {
       this.thumbnailType = ''
     },
     confirmAction() {
-      const editedElement = {
-        id: this.elementKey,
-        name: this.elementName,
-        thumbnail: this.thumbnail,
-        thumbnailType: this.thumbnailType
+      if (this.thumbnailType === 'image') {
+        const editedElement = {
+          id: this.elementKey,
+          name: this.elementName,
+          thumbnail: this.imgValue,
+          thumbnailType: this.thumbnailType
+        }
+        this.$emit('confirm', editedElement)
       }
-      this.$emit('confirm', editedElement)
+      if (this.thumbnailType === 'color') {
+        const editedElement = {
+          id: this.elementKey,
+          name: this.elementName,
+          thumbnail: this.thumbnail,
+          thumbnailType: this.thumbnailType
+        }
+        this.$emit('confirm', editedElement)
+      }
       this.closeDialog()
       this.clearValues()
     },
+    setImage() {
+      this.elementCroppa.generateBlob((blob) => {
+        this.imgValue = blob
+      })
+    },
+    generateElementImage() {
+      this.showInitialImage = false
+      this.elementCroppa.generateBlob(
+        (blob) => {
+          this.thumbnail = URL.createObjectURL(blob)
+          this.imgValue = blob
+        }
+      );
+      this.isImagePreview = true
+      this.elementCroppa.remove()
+    },
+    zoomIn() {
+      this.elementCroppa.zoomIn();
+    },
+    zoomOut() {
+      this.elementCroppa.zoomOut();
+    },
+    rotateAnti() {
+      this.elementCroppa.rotate(-1);
+    },
+    rotate() {
+      this.elementCroppa.rotate();
+    },
+    flipx() {
+      this.elementCroppa.flipX();
+    },
+    flipy() {
+      this.elementCroppa.flipY();
+    },
+    handleFileTypeMismatch(file) {
+      this.$oruga.notification.open({
+        duration: 5000,
+        message: 'Invalid file type. Please choose a jpeg, png or webp file.',
+        position: 'bottom',
+        variant: 'danger',
+        queue: true,
+      });
+    },
+    handleNewImage() {
+      this.showGenerateBtn = true
+    },
+    handleRemoveImage() {
+      this.showGenerateBtn = false
+    },
+    removeImage() {
+      this.thumbnail = ''
+      this.isImagePreview = false;
+    }
   }
 }
 </script>
