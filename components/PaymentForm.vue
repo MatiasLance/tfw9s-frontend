31d75a9c8acac98815ea0b4b5f628ebaa39dbff3
Jonathan Javellana <tech7@sumomedia.co>
2023-01-19 16:26:07 +0900

    Modify ShippingInformationForm Self Pick Up > Set Shipping to 0

--- components/PaymentForm.vue
+++ components/PaymentForm.vue
@@ -146,6 +146,9 @@ export default {
         })
         .then((response) => {
           this.activeStep = 2
+          if (this.shippingInformation.shippingType === 'pickup') {
+            response.shippingCalculation.totalShipping = 0
+          }
           this.$store.commit('cart/setShipping', response.shippingCalculation.totalShipping)
           this.$store.commit('cart/setTotal', response.shippingCalculation.totalProduct)
           this.newSubTotal = (this.subtotal + (this.shipping));
